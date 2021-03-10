719e0d0c2fbf25a36261a6beb9b455ba48a7f177
LorenzoDiGennaro <76122609+LorenzoDiGennaro@users.noreply.github.com>
2021-03-10 16:48:18 +0100

    Add files via upload

---
+++ esercizi2/esercitazione2.1.py
@@ -0,0 +1,10 @@
+import numpy as np
+d1 =input("inserisci 1 numero intero: ")
+d2 =input("inserisci 1 numero intero: ")
+d3 =input("inserisci 1 numero intero: ")
+d4 =input("inserisci 1 numero intero: ")
+d5 =input("inserisci 1 numero intero: ")
+array = [d1, d2, d3, d4, d5]
+array.sort()
+array.reverse() 
+print(array)
